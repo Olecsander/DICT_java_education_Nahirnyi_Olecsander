bd7014b27d5faa20590b324ea2c8e0fc75045a99
Olecsander <o.v.nahirnyi@student.khai.edu>
2024-10-01 11:49:35 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -9,5 +9,10 @@ public class ChatBot {
         System.out.println("Hello! My name is " + botname + ".");
         System.out.println("I was created in " + 2024 + ".");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String UserName = scanner.nextLine();
+        System.out.println("What a great name you have, " + UserName + "!");
+
 
 }
