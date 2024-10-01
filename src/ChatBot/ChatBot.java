ee6073d8b176129cd140fec6e20f3845459e5b8d
Olecsander <o.v.nahirnyi@student.khai.edu>
2024-10-01 11:46:34 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,13 @@
+package ChatBot;
+import java.util.Scanner;
+public class ChatBot {
+
+    public static void main(String[] args) {
+
+        String botname = "Jon";
+
+        System.out.println("Hello! My name is " + botname + ".");
+        System.out.println("I was created in " + 2024 + ".");
+
+
+}
