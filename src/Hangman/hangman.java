3e61f6d622bb24455fb286a8d63e2bc20f47310b
Olecsander <o.v.nahirnyi@student.khai.edu>
2024-10-08 12:31:49 +0200

    Hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -9,3 +9,19 @@ public class hangman {
         boolean playAgain = true;
         Scanner scanner = new Scanner(System.in);
 
+        while (playAgain) {
+            System.out.println("HANGMAN");
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: > ");
+            String userInput = scanner.next();
+
+            if (userInput.equals("play")) {
+                playGame();
+            } else if (userInput.equals("exit")) {
+                System.out.println("Goodbye!");
+                playAgain = false;
+            } else {
+                System.out.println("Invalid input. Please enter \"play\" or \"exit\".");
+            }
+        }
+    }
+
