4796a307b3dd57da72760418e63d38d4e5210d2d
Olecsander <o.v.nahirnyi@student.khai.edu>
2024-10-08 12:29:28 +0200

    Hangman 1-st stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,11 @@
+package Hangman;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class hangman {
+
+    public static void main(String[] args) {
+        boolean playAgain = true;
+        Scanner scanner = new Scanner(System.in);
+
